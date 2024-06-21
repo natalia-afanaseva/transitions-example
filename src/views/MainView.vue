0f9ac2af0f1234c958bc07c774fd6ec13336b416
Natalia A <justnatasha1@gmail.com>
2024-06-21 17:36:34 +0300

    view transitions

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,6 @@
 <template>
-  <RouterView v-slot="{ Component }">
+  <RouterView v-if="isViewTransitionAPISupported" class="child-view" />
+  <RouterView v-slot="{ Component }" v-else>
     <transition :name="transitionName">
       <component class="child-view" :is="Component" />
     </transition>
@@ -7,17 +8,46 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { onBeforeRouteUpdate, RouterView } from 'vue-router'
+import { onMounted, ref } from 'vue'
+import {
+  NavigationGuardNext,
+  onBeforeRouteUpdate,
+  RouteLocationNormalized,
+  RouterView
+} from 'vue-router'
 
 const transitionName = ref('slide-left')
+const isViewTransitionAPISupported = ref(false)
 
-onBeforeRouteUpdate((to, from, next) => {
+function doVueOwnTransition(to: RouteLocationNormalized, from: RouteLocationNormalized) {
   const toDepth = to.path.split('/').length
   const fromDepth = from.path.split('/').length
   transitionName.value = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+}
+
+async function tryViewAPITransition(
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) {
+  if (isViewTransitionAPISupported.value) {
+    // With View Transitions:
+    const transition = document.startViewTransition(() => doVueOwnTransition(to, from, next))
+
+    transition.finished.then(() => {
+      console.log('finished!')
+    })
+  } else {
+    // Fallback for browsers that don't support View Transitions:
+    doVueOwnTransition(to, from)
+  }
+
   next()
+}
+
+onBeforeRouteUpdate(tryViewAPITransition)
+
+onMounted(() => {
+  isViewTransitionAPISupported.value = Boolean(document.startViewTransition)
 })
 </script>
-
-<style scoped></style>
